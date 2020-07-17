f364836c73910815179aef7f2ef78bd65cd9cb8e
Michael Waide <mvwaide@gmail.com>
2020-07-17 14:38:25 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,14 +1,12 @@
 class EmailParser
+attr_accessor :emails
 
-  attr_accessor :email
-  def  initialize(emails)
-    @email = emails
-  end
-
-  def parse
-    email_array = @email.split(/[, ]/).uniq
-    email_array.reject! {|element| element.empty?}
-    email_array
-  end
-
+def initialize(email)
+  @emails = email
 end
+
+def parse
+  emails.delete(',').split.uniq
+end
+
+end 
